f21fb14446081467b78c6eb576e175059ad8f488
Nikolas Papaioannou <nikolas@mnemonic.no>
2022-03-06 18:22:49 +0100

    Adding exports and tests

---
+++ tests/test_export.py
@@ -0,0 +1,39 @@
+import pytest
+
+from random import sample
+from string import ascii_lowercase
+
+from slack_api import Api
+from loguru import logger
+
+from tests.common import auth, config
+
+from export import Export
+
+
+@pytest.fixture(autouse=True, scope="session")
+def export() -> Export:
+    return Export()
+
+
+def test_export(export: Export, config):
+    assert len(export.rooms.keys()) == len(config.channels.cleanup)
+
+
+def test_get_room_json(export: Export, config):
+    # cat * | jq ' . | length' | sort -n | paste -sd+
+    room = export.get_room_json("cyb50")
+    assert len(room) == 1137
+
+
+def test_parse_room(export: Export, config):
+    data = export.parse_room("cyb50")
+    print(data)
+
+
+def test_filter_on_age(export: Export, config):
+    data = export.parse_room("studielabben")
+    assert len(data) == 9
+
+    to_be_removed = export.filter_on_age(data, config.timeout)
+    print(data)
---
+++ tests/test_helpers.py
@@ -0,0 +1,42 @@
+import pytest
+
+from random import sample
+from string import ascii_lowercase
+
+from slack_api import Api
+from loguru import logger
+
+from tests.common import auth, config
+from helpers import get_id, convert_id_to_name
+
+
+def test_get_id_user():
+    raise NotImplementedError
+
+
+def test_get_id_user_list():
+    raise NotImplementedError
+
+
+def test_get_id_channel():
+    raise NotImplementedError
+
+
+def test_get_id_channel_list():
+    raise NotImplementedError
+
+
+def test_get_id_user_fail():
+    raise NotImplementedError
+
+
+def test_get_id_user_list_fail():
+    raise NotImplementedError
+
+
+def test_get_id_channel_fail():
+    raise NotImplementedError
+
+
+def test_get_id_channel_list_fail():
+    raise NotImplementedError
